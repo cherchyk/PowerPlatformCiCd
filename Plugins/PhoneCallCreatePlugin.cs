2d32a1d42142361144cedf84c9741931c5998358
Bohdan C <cherchyk@gmail.com>
2019-06-09 16:26:44 -0400

    new tests

---
+++ Plugins/PhoneCallCreatePlugin.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugins.TypedEntities;
+
+namespace Plugins
+{
+	public class PhoneCallCreatePlugin : IPlugin
+	{
+		public void Execute(IServiceProvider serviceProvider)
+		{
+			#region Boilerplate
+			// Obtain the execution context from the service provider.
+			IPluginExecutionContext context = (IPluginExecutionContext)
+				serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+			// Obtain the organization service reference.
+			IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+			IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+			#endregion
+
+			if (context.InputParameters.Contains("Target") &&
+				context.InputParameters["Target"] is Entity)
+			{
+				var target = context.InputParameters["Target"] as Entity;
+				var phoneCall = target.ToEntity<PhoneCall>();
+
+				var contact = phoneCall.RegardingObjectId;
+				var phoneNumber = phoneCall.PhoneNumber;
+
+				//query to create a phone history only if the pair [phonenumber, contact] doesn't exist
+				using (var ctx = new XrmServiceContext(service))
+				{
+					var exists = (from ph in ctx.CreateQuery<ultra_phonecallhistory>()
+								  where ph.ultra_contactid.Id == contact.Id
+								  where ph.ultra_phonenumber == phoneNumber
+								  select ph).FirstOrDefault() != null;
+
+					if (!exists)
+					{
+						//Create phone history record
+						var phoneHistory = new ultra_phonecallhistory()
+						{
+							ultra_contactid = contact,
+							ultra_phonenumber = phoneNumber,
+							ultra_lastcalldate = DateTime.Now
+						};
+						phoneHistory.Id = service.Create(phoneHistory);
+
+						//Phonecall then assigned to the created phonecall history
+						phoneCall.ultra_phonecallhistoryid = phoneHistory.ToEntityReference();
+
+					}
+
+				}
+
+			}
+		}
+	}
+}
---
+++ PluginsTest/PhoneCallCreatePluginTests.cs
@@ -0,0 +1,79 @@
+﻿using FakeXrmEasy;
+using System;
+using Plugins;
+using Plugins.TypedEntities;
+using Xunit;
+using System.Linq;
+
+namespace D365Saturday.Plugins.Tests
+{
+
+	public class PluginsTests
+	{
+
+		[Fact]
+		public void Should_create_phone_call_history_on_create_of_a_phonecall_with_its_details()
+		{
+			//var ctx = new XrmFakedContext();
+			//Assert.True(false);
+
+
+			//Arrange ==> 
+			//Act  
+			//Assert ==>
+
+			var ctx = new XrmFakedContext();
+			var service = ctx.GetOrganizationService();
+
+
+			var contact = new Contact() { Id = Guid.NewGuid() };
+
+			var phoneCall = new PhoneCall()
+			{
+				Id = Guid.NewGuid(),
+				RegardingObjectId = contact.ToEntityReference(),
+				PhoneNumber = "+34666666666"
+			};
+
+			ctx.ExecutePluginWithTarget<PhoneCallCreatePlugin>(phoneCall);
+
+			var historyRecords = ctx.CreateQuery<ultra_phonecallhistory>().ToList();
+			Assert.Equal(1, historyRecords.Count);
+
+			var historyRecord = historyRecords.First();
+			Assert.Equal(phoneCall.PhoneNumber, historyRecord.ultra_phonenumber);
+			Assert.Equal(phoneCall.RegardingObjectId.Id, historyRecord.ultra_contactid.Id);
+			Assert.Equal(historyRecord.Id, phoneCall.ultra_phonecallhistoryid.Id);
+
+		}
+
+		[Fact]
+		public void Shouldnt_create_a_duplicate_phone_call_history_record()
+		{
+			var ctx = new XrmFakedContext();
+
+			var contact = new Contact() { Id = Guid.NewGuid() };
+
+			var phoneCall = new PhoneCall()
+			{
+				Id = Guid.NewGuid(),
+				RegardingObjectId = contact.ToEntityReference(),
+				PhoneNumber = "+34666666666"
+			};
+
+			var existingPhoneCallHistory = new ultra_phonecallhistory()
+			{
+				Id = Guid.NewGuid(),
+				ultra_contactid = contact.ToEntityReference(),
+				ultra_phonenumber = phoneCall.PhoneNumber
+			};
+			ctx.Initialize(existingPhoneCallHistory);
+
+			ctx.ExecutePluginWithTarget<PhoneCallCreatePlugin>(phoneCall);
+
+			var historyRecords = ctx.CreateQuery<ultra_phonecallhistory>().ToList();
+			Assert.Single(historyRecords);
+		}
+
+	}
+}
