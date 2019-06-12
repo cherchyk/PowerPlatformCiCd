abe40e185ebaf9d72034fd835970d6be9289da42
Bohdan C <cherchyk@gmail.com>
2019-06-12 17:45:39 -0400

    local tools

---
+++ alm/connectDialog.ps1
@@ -0,0 +1,31 @@
+### CONNECT
+$connectionHost = "https://cibc-hs-2.crm3.dynamics.com/"
+$organizationName = "CIBC Horizon&Smart 2"
+$DeploymentRegion = "CAN"
+
+
+#works
+#$cred = Get-Credential
+
+#$CRMOrgs = Get-CrmOrganizations -ServerUrl $connectionHost -Credential $Cred
+#$CRMOrgs  
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+#Works
+$Global:CRMConn = Get-CrmConnection -InteractiveMode
+
+#$Global:CRMConn = Get-CrmConnection -ServerUrl $connectionHost -Credential $Cred -OrganizationName $organizationName
+#$Global:CRMConn = Get-CrmConnection -OrganizationName $organizationName -Credential $cred -DeploymentRegion $DeploymentRegion -OnlineType Office365  
+#$CRMConn = Get-CrmConnection -Credential $cred -ServerUrl $connectionHost -OrganizationName $organizationName
+#$CRMConn = Get-CrmConnection -ConnectionString "Url=https://cibcdemo2.crm3.dynamics.com/;UserName=sssssss;Password=ssssssssssss"
+####Connect-CrmOnline -Credential $cred -ServerUrl $connectionHost -OrganizationName $organizationName
+#Get-CrmConnection -OrganizationName $organizationName -OnLineType Office365 -Credential $cred
+#$CRMOrgs = Get-CrmOrganizations -Credential $cred -DeploymentRegion CAN -OnlineType Office365
+
+
+Write-Host "Successfully connected." -ForegroundColor Green
+Write-Host ("   User: " + $cred.UserName) -ForegroundColor Green
+Write-Host ("   Org: " + $CRMConn.CrmConnectOrgUriActual) -ForegroundColor Green
+$CRMConn
+##########################################################################################
---
+++ alm/crm2local.ps1
@@ -0,0 +1,15 @@
+### crm2local
+$solutionName = "almdemo"
+
+$solutionDir = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "..\"))
+#$slnZip = $solutionName + ".zip"
+$slnZip = "solution.zip"
+
+Write-Host ("Org: " + $CRMConn.CrmConnectOrgUriActual) -ForegroundColor Green
+Write-Host "Solution Name: " $solutionName -ForegroundColor Green
+Write-Host "Solution Dir: " $solutionDir -ForegroundColor Green
+Write-Host "Solution file: " $slnZip -ForegroundColor Green
+
+Export-CrmSolution -conn $CRMConn -SolutionName $solutionName -SolutionFilePath $solutionDir -SolutionZipFileName $slnZip
+##########################################################################################
+
