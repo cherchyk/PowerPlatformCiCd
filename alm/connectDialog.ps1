021c5a703ced0c8e7b54d6ea9c265ae1e26b6bf2
Bohdan C <cherchyk@gmail.com>
2019-06-12 17:56:00 -0400

    cleaning

--- alm/connectDialog.ps1
+++ alm/connectDialog.ps1
@@ -1,20 +1,19 @@
 ### CONNECT
-$connectionHost = "https://cibc-hs-2.crm3.dynamics.com/"
-$organizationName = "CIBC Horizon&Smart 2"
-$DeploymentRegion = "CAN"
-
-
-#works
-#$cred = Get-Credential
-
-#$CRMOrgs = Get-CrmOrganizations -ServerUrl $connectionHost -Credential $Cred
-#$CRMOrgs  
-
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 #Works
 $Global:CRMConn = Get-CrmConnection -InteractiveMode
 
+#$connectionHost = "https://cibc-hs-2.crm3.dynamics.com/"
+#$organizationName = "CIBC Horizon&Smart 2"
+#$DeploymentRegion = "CAN"
+
+#works
+#$cred = Get-Credential
+#$CRMOrgs = Get-CrmOrganizations -ServerUrl $connectionHost -Credential $Cred
+#$CRMOrgs  
+
+#issues
 #$Global:CRMConn = Get-CrmConnection -ServerUrl $connectionHost -Credential $Cred -OrganizationName $organizationName
 #$Global:CRMConn = Get-CrmConnection -OrganizationName $organizationName -Credential $cred -DeploymentRegion $DeploymentRegion -OnlineType Office365  
 #$CRMConn = Get-CrmConnection -Credential $cred -ServerUrl $connectionHost -OrganizationName $organizationName
@@ -22,7 +21,6 @@ $Global:CRMConn = Get-CrmConnection -InteractiveMode
 ####Connect-CrmOnline -Credential $cred -ServerUrl $connectionHost -OrganizationName $organizationName
 #Get-CrmConnection -OrganizationName $organizationName -OnLineType Office365 -Credential $cred
 #$CRMOrgs = Get-CrmOrganizations -Credential $cred -DeploymentRegion CAN -OnlineType Office365
-
 
 Write-Host "Successfully connected." -ForegroundColor Green
 Write-Host ("   User: " + $cred.UserName) -ForegroundColor Green
